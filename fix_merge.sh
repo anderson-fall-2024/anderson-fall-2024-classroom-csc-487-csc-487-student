828c5f377ba38019939d17c6a351a2a646d3f1c3
pauleanderson <pauleanderson@gmail.com>
2024-10-17 07:54:54 -0700

    update

--- fix_merge.sh
+++ fix_merge.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 for f in $(grep -Rl '^>>>>>>> ' --include="*.php" --include="*.css" --include="*.js" --include="*.html" --include="*.svg" --include="*.txt" --include="*.py" --include="*.ipynb" .)
 do
-sed -i -e '/^=======/,/^>>>>>>> /d' -e '/^<<<<<<< /d' $f
+sed -i -e '/^=======/,/^>>>>>>> /d' $f
+sed -i -e '/^<<<<<<< /d' $f
 sed -i -e '/^>>>>>>> /d' $f
 echo "$f Fixed"
 done
